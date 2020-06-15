37ad873a61f0b782f359d0dd311a7fec25b929a1
Youngwan Kim <y103.kim@gmail.com>
2020-06-16 00:32:38 +0900

    Setup prezto
    Signed-off-by: Youngwan Kim <y103.kim@gmail.com>

--- setup.sh
+++ setup.sh
@@ -14,6 +14,23 @@ ENV=$(dirname $(realpath ${BASH_SOURCE[0]}))
 echo ENV=$ENV
 cd $HOME
 
+# zsh
+read -r -d '' ZSH_SETUP << EOM
+if [ ! -d ${ZDOTDIR:-$HOME}/.zprezto ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
+git --git-dir=${ZDOTDIR:-$HOME}/.zprezto/.git pull origin master
+cp ~/.zprezto/runcoms/zlogin ~/.zlogin
+cp ~/.zprezto/runcoms/zlogout ~/.zlogout
+cp ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
+cp ~/.zprezto/runcoms/zshenv ~/.zshenv
+cp ~/.zprezto/runcoms/zprofile ~/.zprofile
+cp ~/.zprezto/runcoms/zshrc ~/.zshrc
+EOM
+zsh -c "$ZSH_SETUP"
+sed -i 's/sorin/powerlevel10k/g' ~/.zpreztorc
+cat $ENV/zshrc-extend >> ~/.zshrc
+
 # TMUX
 [ ! -d ~/.tmux ] && git clone https://github.com/gpakosz/.tmux.git
 git --git-dir=$HOME/.tmux/.git pull origin master
