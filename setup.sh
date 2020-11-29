bc111881b4b2af0566b6cfaafb577047c6b246fa
Youngwan Kim <y103.kim@gmail.com>
2020-11-29 18:15:13 +0900

    setup: change order of Vundle update and install

--- setup.sh
+++ setup.sh
@@ -68,5 +68,5 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 cp $ENV/vimrc ~/.vimrc
-vim +PluginUpdate +qall
 vim +PluginInstall +qall
+vim +PluginUpdate +qall
