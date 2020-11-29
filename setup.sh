2aa0b9c6aa370e9dae2cfad454c3e5c8b0d9a50a
Youngwan Kim <y103.kim@gmail.com>
2020-11-29 17:54:45 +0900

    setup: change order of setup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-# Setup packages for ubuntu 20.04
-if [ $(lsb_release -d | grep -c "Ubuntu") = 1 ]; then
-  if [ $(dpkg -l | grep -c build-essential) = 0 ]; then
-    sudo apt-get update
-    sudo apt-get install -y build-essential python3-venv vim git zsh xclip tree
-  fi
-  [ "$SHELL" != "/bin/zsh" ] && sudo chsh -s /bin/zsh $USER
-fi
-
 # Go to Home
 ENV=$(dirname $(realpath ${BASH_SOURCE[0]}))
 echo ENV=$ENV
 cd $HOME
 
+#########################  Archive Setup ##########################
+# Create env base
+BASEPATH=$HOME/env
+ARCHIVE=$HOME/env/archive
+mkdir -p $HOME/env/bin
+NODE_VERSION="v14.5.0"
+GO_VERSION="1.14.6"
+
+# Nodejs
+mkdir -p $BASEPATH/node
+NODE_TAR=$ARCHIVE/node-$NODE_VERSION-linux-x64.tar.xz
+if [ ! -f $NODE_TAR ]; then
+  wget https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x65.tar.xz -P $ARCHIVE
+fi
+tar xf $NODE_TAR -C $BASEPATH/node --strip-components=1
+
+# Golang
+mkdir -p $BASEPATH/go
+GO_TAR=$ARCHIVE/go$GO_VERSION.linux-amd64.tar.gz
+if [ ! -f $GO_TAR ]; then
+  wget https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz -P $ARCHIVE
+fi
+tar xf $GO_TAR -C $BASEPATH/go --strip-components=1
+
+
+#########################  Install pulgins ##########################
 # zsh
 read -r -d '' ZSH_SETUP << EOM
 if [ ! -d ${ZDOTDIR:-$HOME}/.zprezto ]; then
@@ -53,26 +70,3 @@ fi
 cp $ENV/vimrc ~/.vimrc
 vim +PluginUpdate +qall
 vim +PluginInstall +qall
-
-# Create env base
-BASEPATH=$HOME/env
-ARCHIVE=$HOME/env/archive
-mkdir -p $HOME/env/bin
-
-# Nodejs
-mkdir -p $BASEPATH/node
-NODE_VERSION="v14.5.0"
-NODE_TAR=$ARCHIVE/node-$NODE_VERSION-linux-x64.tar.xz
-if [ ! -f $NODE_TAR ]; then
-  wget https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz -P $ARCHIVE
-fi
-tar xf $NODE_TAR -C $BASEPATH/node --strip-components=1
-
-# Golang
-GO_VERSION="1.14.6"
-mkdir -p $BASEPATH/go
-GO_TAR=$ARCHIVE/go$GO_VERSION.linux-amd64.tar.gz
-if [ ! -f $GO_TAR ]; then
-  wget https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz -P $ARCHIVE
-fi
-tar xf $GO_TAR -C $BASEPATH/go --strip-components=1
