198a1ee90148bdc7fa1874d6a976e2ef05a0970f
Youngwan Kim <y103.kim@gmail.com>
2020-12-01 02:50:28 +0900

    install diff-so-fancy

--- setup.sh
+++ setup.sh
@@ -166,5 +166,25 @@ if [ "$2" == "re" ] ; then
   vim +PluginUpdate +qall
 fi
 
+# diff-so-fancy
+[ ! -d $BASEPATH/diff-so-fancy ] && git clone https://github.com/so-fancy/diff-so-fancy.git $BASEPATH/diff-so-fancy
+pushd $BASEPATH/diff-so-fancy
+git checkout .
+git pull origin master
+cp -rf lib diff-so-fancy $BASEPATH/usr/bin
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+popd
+
 # setup KEYWORDS
 cp $ENV/KEYWORDS $BASEPATH
