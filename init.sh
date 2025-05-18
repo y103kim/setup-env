ff3f21c444b1f118c4df429a405f99a88f3a9d84
Youngwan Kim <y103.kim@gmail.com>
2025-05-18 00:24:41 -0400

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,20 @@
-## lets assume brew is installed
+#!/bin/bash
 
+# Find brew path
+BREW_PATHS=(
+  "$HOME/.linuxbrew/bin"
+  "/home/linuxbrew/.linuxbrew/bin"
+  "/opt/homebrew/bin"
+)
+
+for DIR in "${BREW_PATHS[@]}"; do
+  if [ -x "$DIR/brew" ]; then
+    export PATH="$DIR:$PATH"
+    break
+  fi
+done
+
+# install essential packages
 brew install nushell
 brew install neovim
 brew install ripgrep
