643439757201672071beef4319a842a1eb864e8a
Youngwan Kim <y103.kim@gmail.com>
2025-05-20 00:41:05 -0400

    install bufdel

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -1,20 +1,19 @@
 require("config.lazy")
 
 -- default setup
-vim.o.autoindent = true
-vim.o.cindent = true
-vim.o.smartindent = true
-
-vim.o.number = true
-vim.o.relativenumber = true
-
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.expandtab = true
-vim.o.undofile = true
-
+vim.opt.autoindent = true
+vim.opt.cindent = true
+vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.undofile = true
 vim.opt.termguicolors = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
 
 -- basic key maps
 vim.keymap.set("n", "<C-h>", "<cmd>BufferLineCyclePrev<cr>")
@@ -22,11 +21,11 @@ vim.keymap.set("n", "<C-l>", "<cmd>BufferLineCycleNext<cr>")
 vim.keymap.set('n', '<C-j>', '<C-w>w')
 vim.keymap.set('n', '<C-k>', '<C-w>W')
 vim.keymap.set("n", "<leader>cv", ":FzfLua files search_paths=~/.config/nvim/lua fd_opt='-e lua' query=lua<CR>")
+vim.keymap.set("n", "<leader>q", ":BufDel<cr>")
 
 -- set system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- goto
 vim.keymap.set("n", "<leader>gi", vim.diagnostic.open_float)
--- nvim/.config/nvim/lua/plugins/common.lua
+++ nvim/.config/nvim/lua/plugins/common.lua
@@ -1,25 +1,26 @@
 return {
   { "folke/which-key.nvim" },
   { "mbbill/undotree" },
+  { 'ojroques/nvim-bufdel' },
   {
     'gelguy/wilder.nvim',
     config = function ()
-	    local wilder = require('wilder')
-	    wilder.setup({modes = {':', '/', '?'}})
+      local wilder = require('wilder')
+      wilder.setup({modes = {':', '/', '?'}})
 
-	    wilder.set_option('pipeline', {
-		    wilder.branch(
-			    wilder.cmdline_pipeline(),
+      wilder.set_option('pipeline', {
+        wilder.branch(
+          wilder.cmdline_pipeline(),
           wilder.python_search_pipeline({
             pattern = 'fuzzy',
           })
-		    ),
-	    })
+        ),
+      })
       wilder.set_option('renderer', wilder.popupmenu_renderer(
         wilder.popupmenu_border_theme({
-        highlighter = wilder.basic_highlighter(),
-        left = {' ', wilder.popupmenu_devicons()},
-        right = {' ', wilder.popupmenu_scrollbar()},
+          highlighter = wilder.basic_highlighter(),
+          left = {' ', wilder.popupmenu_devicons()},
+          right = {' ', wilder.popupmenu_scrollbar()},
           border = 'rounded',
         })
       ))
