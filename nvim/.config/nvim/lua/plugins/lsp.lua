b7e9be63e4608f52f5e19fbc5c4929c8029c8168
Youngwan Kim <y103.kim@gmail.com>
2025-05-19 23:31:10 -0400

    replace autocmp to nvim-cmp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,18 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "mason-org/mason-lspconfig.nvim",
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup()
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = "nushell/tree-sitter-nu",
+    config = function ()
+      require("nvim-treesitter.configs").setup({
+        modules = {},
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "nu", "python", "javascript", "html", "typescript", "nu" },
+        ignore_install = {},
+        auto_install = true,
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
     end
   },
   {
@@ -22,25 +27,83 @@ return {
     },
   },
   {
-    "saghen/blink.cmp",
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
-        },
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      {
+        "hrsh7th/nvim-cmp",
+        opts = function(_, opts)
+          opts.sources = opts.sources or {}
+          table.insert(opts.sources, {
+            name = "lazydev",
+            group_index = 0,
+          })
+        end,
       },
-      keymap = {
-        preset = 'default',
-        ['<Tab>'] = { 'accept' },
-      }
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "j-hui/fidget.nvim",
     },
-    version = "*",
-  }
+    config = function()
+      require("mason").setup()
+      require("lspconfig").nushell.setup({
+        cmd = { "nu", "--lsp" },
+        filetypes = { "nu" },
+        single_file_support = true,
+      })
+
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        require("cmp_nvim_lsp").default_capabilities()
+      )
+
+      require("fidget").setup({})
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        automatic_enable = true,
+        ensure_installed = {},
+        handlers = {
+          function(server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {
+              capabilities = capabilities
+            }
+          end,
+        }
+      })
+
+      local cmp = require('cmp')
+      local cmp_select = { behavior = cmp.SelectBehavior.Select }
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'path' },
+          { name = 'buffer' },
+        })
+      })
+
+      vim.diagnostic.config({
+        -- update_in_insert = true,
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          header = "",
+          prefix = "",
+        },
+      })
+    end
+  },
 }
