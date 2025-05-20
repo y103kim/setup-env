621258d5bfd978b571c2dfe8b9ae9b8a987fa845
Youngwan Kim <y103.kim@gmail.com>
2025-05-19 20:15:56 -0400

    move logic to init.sh and  lean up setup.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Find brew path
 BREW_PATHS=(
+  # Linux brew path without root
   "$HOME/.linuxbrew/bin"
+  # Linux brew path with root
   "/home/linuxbrew/.linuxbrew/bin"
+  # MacOS brew path
   "/opt/homebrew/bin"
 )
 
+# Find brew path
 for DIR in "${BREW_PATHS[@]}"; do
   if [ -x "$DIR/brew" ]; then
     export PATH="$DIR:$PATH"
@@ -15,5 +18,11 @@ for DIR in "${BREW_PATHS[@]}"; do
 done
 
 # install essential packages
-brew install nushell neovim ripgrep zellij nvm fzf starship
+brew install nushell neovim ripgrep zellij nvm fzf starship ripgreg fd
 
+# copy keymap for macOS
+if [[ $OSTYPE == 'darwin'* ]]; then
+  echo 'It is macOS'
+  mkdir -p $HOME/Library/KeyBindings
+  cp DefaultKeyBinding.dict $HOME/Library/KeyBindings
+fi
