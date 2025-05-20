227d2ca96e8a6d8b2d3925738db231f660c9018b
Youngwan Kim <y103.kim@gmail.com>
2025-05-19 23:31:23 -0400

    install wilder

--- nvim/.config/nvim/lua/plugins/common.lua
+++ nvim/.config/nvim/lua/plugins/common.lua
@@ -1,4 +1,28 @@
 return {
   { "folke/which-key.nvim" },
   { "mbbill/undotree" },
+  {
+    'gelguy/wilder.nvim',
+    config = function ()
+	    local wilder = require('wilder')
+	    wilder.setup({modes = {':', '/', '?'}})
+
+	    wilder.set_option('pipeline', {
+		    wilder.branch(
+			    wilder.cmdline_pipeline(),
+          wilder.python_search_pipeline({
+            pattern = 'fuzzy',
+          })
+		    ),
+	    })
+      wilder.set_option('renderer', wilder.popupmenu_renderer(
+        wilder.popupmenu_border_theme({
+        highlighter = wilder.basic_highlighter(),
+        left = {' ', wilder.popupmenu_devicons()},
+        right = {' ', wilder.popupmenu_scrollbar()},
+          border = 'rounded',
+        })
+      ))
+    end
+  },
 }
